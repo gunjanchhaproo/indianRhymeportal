568c76d490bf40ec75297715bbd0e8e25d0dcdad
gunjankc212@gmail.com <gunjankc212@gmail.com>
2023-03-13 00:27:50 +0530

    Done with video

--- src/app/poetry/viewpoetry/viewpoetry.component.ts
+++ src/app/poetry/viewpoetry/viewpoetry.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { PoetryService } from '../poetry.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-viewpoetry',
@@ -12,6 +14,8 @@ export class ViewpoetryComponent implements OnInit {
   poemForm!:FormGroup
   poems:any=[]
   selectedPoem:any
+  data:any
+  downloadClicked:boolean=false
 
   constructor(private poemService:PoetryService) { }
 
@@ -35,6 +39,26 @@ export class ViewpoetryComponent implements OnInit {
       });
       console.log(value)
     })
+  }
+
+  public openPDF(): void {
+    this.downloadClicked=true
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('angular-demo.pdf');
+    });
+  }
+
+  playPause() {
+    var myVideo: any = document.getElementById("my_video_1");
+    if (myVideo.paused) myVideo.play();
+    else myVideo.pause();
   }
 
 }
